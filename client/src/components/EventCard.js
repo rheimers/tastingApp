18ccab06d0333a089ff58aa9373137d09540d1fb
rheimers <regineheimers@gmail.com>
2020-09-22 22:45:50 +0200

    Add date object and methods, optimize grid

--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -4,10 +4,9 @@ import PropTypes from "prop-types";
 
 const Card = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 33.3%);
-  grid-template-rows: repeat(4, 25%);
+  grid-template-columns: repeat(3, calc(100% / 3));
+  grid-template-rows: repeat(3, calc(100% / 3));
   background-image: url(${({ imgSrc }) => imgSrc});
-  /*contain vs. cover*/
   background-size: contain;
   background-repeat: no-repeat;
   width: 108px;
@@ -15,8 +14,8 @@ const Card = styled.div`
   border-radius: 4px;
   h2 {
     color: var(--highlight-clr-wine);
-    grid-row: 3 / 4;
-    grid-column: 1 / 4;
+    grid-row: 3;
+    grid-column: 1 / 3;
     font-size: 12px;
   }
 `;
@@ -28,12 +27,31 @@ const DateContainer = styled.div`
   border-radius: 4px;
   text-align: center;
   color: var(--clr-primary);
+  margin: 4px;
+  font-size: 33%;
 `;
-
+const days = ["Mo", "Tu", "Wed", "Thu", "Fr", "Sat", "Sun"];
+const months = [
+  "Jan",
+  "Feb",
+  "March",
+  "Apr",
+  "May",
+  "June",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 export default function EventCard({ title, imgSrc, date }) {
   return (
     <Card imgSrc={imgSrc}>
-      <DateContainer>{date}</DateContainer>
+      <DateContainer>
+        <div>{days[date.getDay()]}</div>
+        <div>{date.getDate()}</div>
+        <div>{months[date.getMonth()]}</div>
+      </DateContainer>
       <h2>{title}</h2>
     </Card>
   );
@@ -42,5 +60,5 @@ export default function EventCard({ title, imgSrc, date }) {
 EventCard.propTypes = {
   title: PropTypes.string.isRequired,
   imgSrc: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
+  date: PropTypes.instanceOf(Date).isRequired,
 };
--- client/src/stories/EventCard.stories.js
+++ client/src/stories/EventCard.stories.js
@@ -9,14 +9,14 @@ export default {
 export const WineEventCard = () => (
   <EventCard
     title="Riesling with Pete"
-    date="SA 20 Oct"
+    date={new Date("2020-09-22T15:12:23.263Z")}
     imgSrc="https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
   />
 );
 export const BeerEventCard = () => (
   <EventCard
     title="Riesling with Pete"
-    date="SA 20 Oct"
+    date={new Date("2020-10-22T15:12:23.263Z")}
     imgSrc="https://images.unsplash.com/photo-1541692641319-981cc79ee10a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
   />
 );
