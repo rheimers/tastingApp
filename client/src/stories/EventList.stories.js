d4de41eb68f8c1795335844af5aa9985863b773d
rheimers <regineheimers@gmail.com>
2020-09-23 19:54:57 +0200

    Add EventList.stories.js

---
+++ client/src/stories/EventList.stories.js
@@ -0,0 +1,202 @@
+import React from "react";
+import EventList from "../components/EventList";
+
+export default {
+  title: "EventList",
+  component: EventList,
+};
+
+export const UpcomingEvents = () => (
+  <>
+    <EventList
+      title={`Next tastings`}
+      items={[
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+      ]}
+    />
+    <EventList
+      title={`Your Taste`}
+      items={[
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          title: `Riesling with Pete`,
+          date: "2020-10-22T15:12:23.263Z",
+          imgSrc:
+            "https://images.unsplash.com/photo-1591902318851-0073633a96a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        },
+      ]}
+    />
+  </>
+);
