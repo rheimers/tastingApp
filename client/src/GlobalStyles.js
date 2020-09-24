4cbabaa7917c6c6db2fa221be4272a17b52a5d65
rheimers <regineheimers@gmail.com>
2020-09-24 12:45:12 +0200

    Adapt Global Styles

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -16,7 +16,7 @@ const GlobalStyles = () => {
         }
         :root {
           --clr-primary: #ffa200;
-          --highlight-clr-wine: #fa20d8f2;
+          --highlight-clr-category: #fa20d8f2;
           --font-color-white: #fffdfd;
           --contrast-color-lightgrey: #dddd;
           --page-background-color: #262524;
