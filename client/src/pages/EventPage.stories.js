c3e572494641c0bee14f52ade4c000be2362fcd7
rheimers <regineheimers@gmail.com>
2020-09-25 10:25:25 +0200

    Fill EventPage.stories.js

---
+++ client/src/pages/EventPage.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import EventPage from "../pages/EventPage";
+
+export default {
+  title: "EventPage",
+  component: EventPage,
+};
+
+export const EventHomePage = () => <EventPage />;
