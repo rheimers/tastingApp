423e0db22dec17619e38d3aabc559138abd41065
rheimers <regineheimers@gmail.com>
2020-09-25 07:18:43 +0200

    Add router and middlewares

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.use(
   express.static(path.join(__dirname, "client/storybook-static"))
 );
 
+app.use(router);
+app.use(middlewares);
+
 //Handle React routing, return all requests to React app
 app.get("/", (request, response) => {
   response.send("Hello World!");
