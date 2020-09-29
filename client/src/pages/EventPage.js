c009a0246305283ef3277e4cdceddc0be7e17032
rheimers <regineheimers@gmail.com>
2020-09-29 17:20:42 +0200

    Add Filter.js

--- client/src/pages/EventPage.js
+++ client/src/pages/EventPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Searchbar from "../components/Searchbar";
 import EventList from "../components/EventList";
+import FilterList from "../components/FilterList";
 import { getEvents } from "../api/getEvents";
 import useAsync from "../hooks/useAsync";
 import Footer from "../components/Footer";
@@ -13,6 +14,7 @@ function EventPage() {
       <Searchbar></Searchbar>
       <EventList title="Next tastings" events={events}></EventList>
       <EventList title="For your taste" events={events}></EventList>
+      <FilterList title="Taste by beverage"></FilterList>
       <Footer></Footer>
     </div>
   );
