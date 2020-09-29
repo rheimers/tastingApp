ff933227d3aac855918c879b088f9b85f8d0b473
rheimers <regineheimers@gmail.com>
2020-09-29 19:43:12 +0200

    Add styling to Eventpage

--- client/src/pages/EventPage.js
+++ client/src/pages/EventPage.js
@@ -5,18 +5,25 @@ import FilterList from "../components/FilterList";
 import { getEvents } from "../api/getEvents";
 import useAsync from "../hooks/useAsync";
 import Footer from "../components/Footer";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: column;
+  margin-left: 0.8em;
+`;
 
 function EventPage() {
   const { data: events } = useAsync(getEvents);
   console.log(events);
   return (
-    <div>
+    <Container>
       <Searchbar></Searchbar>
       <EventList title="Next tastings" events={events}></EventList>
       <EventList title="For your taste" events={events}></EventList>
       <FilterList title="Taste by beverage"></FilterList>
       <Footer></Footer>
-    </div>
+    </Container>
   );
 }
 
