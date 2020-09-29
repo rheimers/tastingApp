6d9ed85892542eccd500ca364d51ea1fe4c29091
rheimers <regineheimers@gmail.com>
2020-09-29 19:42:37 +0200

    Add FilterList.stories

---
+++ client/src/stories/FilterList.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FilterList from "../components/FilterList";
+
+export default {
+  title: "FilterList",
+  component: FilterList,
+};
+
+const Template = () => <FilterList />;
+
+export const BeverageFilterList = Template.bind({});
