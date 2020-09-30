1b4b7fd5840366de97d77897347ec1ad27012e1b
rheimers <regineheimers@gmail.com>
2020-09-30 18:50:23 +0200

    Setup routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
-//import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
 import EventPage from "./pages/EventPage";
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyles />
-      <EventPage></EventPage>
-    </>
+      <Switch>
+        <Route exact path="/">
+          <EventPage></EventPage>
+        </Route>
+        <Route path="/search"></Route>
+      </Switch>
+    </Router>
   );
 }
 
