4a5d9f4644cc5b2e51ecfd66a386cfdece48d2de
rheimers <regineheimers@gmail.com>
2020-10-01 19:17:54 +0200

    Modify API - make any query

--- client/src/api/getFilteredEvents.js
+++ client/src/api/getFilteredEvents.js
@@ -1,5 +1,5 @@
 export const getFilteredEvents = async (query) => {
-  const response = await fetch(`/api/events?title_like=${query}`);
+  const response = await fetch(`/api/events?q=${query}`);
   if (!response.ok) {
     throw response;
   }
