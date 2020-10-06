dbf3e5303357698b782a146d9623f3067a085e86
rheimers <regineheimers@gmail.com>
2020-10-06 11:51:52 +0200

    Add grid to header

--- client/src/components/EventDetailsHeader.js
+++ client/src/components/EventDetailsHeader.js
@@ -5,27 +5,44 @@ import ThirstIcon from "../assets/icons/thirst.svg";
 import ArrowLeftIcon from "../assets/icons/arrowleft.svg";
 import ShareIcon from "../assets/icons/share.svg";
 
-const HeaderContainer = styled.div`
-  display: grid;
-`;
-
 const Header = styled.header`
+  display: grid;
+  grid-template-columns: 50px auto 50px 50px;
+  grid-template-rows: repeat(4, 25%);
   background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.5)),
     url(${EspressoImg});
   background-repeat: no-repeat;
   background-position: 50% 30%;
   background-size: 100%;
-  height: 174px;
+  height: 100%;
+  min-height: 174px;
+
+  img:first-of-type {
+    grid-column: 1/2;
+    grid-row: 2/3;
+    justify-self: center;
+  }
+  img:nth-of-type(2) {
+    grid-column: 3/4;
+    grid-row: 1/2;
+    align-self: center;
+    justify-self: center;
+  }
+  img:nth-of-type(3) {
+    grid-column: 4/5;
+    grid-row: 1/2;
+    align-self: center;
+    justify-self: center;
+  }
 `;
 
 export default function DetailsHeader() {
   return (
-    <HeaderContainer>
-      <Header>
-        <img src={ArrowLeftIcon} alt="Arrow back" />
-        <img src={ShareIcon} alt="Share" />
-        <img src={ThirstIcon} alt="Thirst" />
-      </Header>
-    </HeaderContainer>
+    <Header>
+      <img src={ArrowLeftIcon} alt="Arrow back" />
+
+      <img src={ShareIcon} alt="Share" />
+      <img src={ThirstIcon} alt="Thirst" />
+    </Header>
   );
 }
