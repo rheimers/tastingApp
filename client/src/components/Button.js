909bb9e57cceb8c65247c895ef127341b93537ea
rheimers <regineheimers@gmail.com>
2020-10-06 11:51:19 +0200

    Finalize styling button

---
+++ client/src/components/Button.js
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+  padding: 0.8rem 1rem 0.8rem;
+  border-radius: 0.8rem;
+  min-width: 284px;
+  max-width: 400px;
+  background-color: #ffa200;
+  color: var(--font-color-white);
+  text-transform: uppercase;
+  border: none;
+  width: 100%;
+`;
+
+export default Button;
--- client/src/components/EventDetailsFooter.js
+++ client/src/components/EventDetailsFooter.js
@@ -1,24 +1,15 @@
 import React from "react";
 import styled from "@emotion/styled";
+import Button from "./Button.js";
 
 const FooterContainer = styled.footer`
-  display: grid;
+  display: flex;
+  justify-content: center;
+  margin: 1rem 0;
   position: fixed;
   left: 0;
   bottom: 0;
-  width: 100vw;
-  grid-template-columns: 1fr;
-  grid-template-rows: auto 1fr auto;
-`;
-
-const Button = styled.button`
-  margin: 2.8rem;
-  padding: 0.8rem 1rem 0.8rem;
-  border-radius: 0.8rem;
-  min-width: 284px;
-  background-color: #ffa200;
-  color: var(--font-color-white);
-  text-transform: uppercase;
+  width: 100%;
 `;
 
 export default function Footer() {
