e6ee02296e1bdd18107d67f1c6274dd32c210a4f
rheimers <regineheimers@gmail.com>
2020-10-07 22:30:48 +0200

    Handle input of searchbar on SearchPageHeader

--- client/src/components/SearchPageHeader.js
+++ client/src/components/SearchPageHeader.js
@@ -1,20 +1,29 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 import Searchbar from "../components/Searchbar";
 import styled from "@emotion/styled";
 import ArrowLeft from "../assets/icons/arrowleft.svg";
-import CancelIcon from "../assets/icons/cancel.svg";
+import { useHistory } from "react-router-dom";
 
 const Header = styled.header`
   display: flex;
 `;
 
-function SearchPageHeader(props) {
+function SearchPageHeader() {
+  const history = useHistory();
+  const [query, setQuery] = useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    history.push(`/search?q=${query}`);
+  }
   return (
     <Header>
       <img src={ArrowLeft} alt="Arrow back" />
-      <Searchbar></Searchbar>
-      <img src={CancelIcon} alt="Cancel" />
+      <Searchbar
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+        onSubmit={handleSubmit}
+        onClear={() => setQuery("")}
+      />
     </Header>
   );
 }
