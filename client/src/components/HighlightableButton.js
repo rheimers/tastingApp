6d6de9522d29272b3285e218bab9c0c012ee78ed
rheimers <regineheimers@gmail.com>
2020-10-07 21:15:23 +0200

    Highlight icons in header onClick

---
+++ client/src/components/HighlightableButton.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+
+function HighlightableButton({ children, className }) {
+  const [clicked, setClicked] = useState(false);
+  return (
+    <button
+      className={`${className} ${clicked ? "active" : ""}`}
+      onClick={() => setClicked(true)}
+    >
+      {children}
+    </button>
+  );
+}
+
+const StyledHighlightableButton = styled(HighlightableButton)`
+  background: transparent;
+  border: none;
+  &.active {
+    background: rgba(255, 255, 255, 0.2);
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+export default StyledHighlightableButton;
+HighlightableButton.propTypes = {
+  children: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+};
