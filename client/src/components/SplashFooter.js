916a534790825925e030dd7b9ff0971c1a2af2bf
rheimers <regineheimers@gmail.com>
2020-10-10 07:44:07 +0200

    Create Footer SplashPage1

---
+++ client/src/components/SplashFooter.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Progress1Src from "../assets/icons/progress1.svg";
+import ArrowRightSrc from "../assets/icons/arrowright.svg";
+
+const FooterContainer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100vw;
+`;
+
+const FooterIcons = styled.nav`
+  padding: 1em 0.5em;
+  margin: 0 1.5em;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const FooterIcon = styled.div`
+  img {
+    margin-bottom: 0.3rem;
+  }
+  div {
+    margin-top: 0.5rem;
+  }
+`;
+
+export default function Footer() {
+  return (
+    <FooterContainer>
+      <FooterIcons>
+        <FooterIcon>
+          <img src={Progress1Src} alt="Progressbar 1" />
+        </FooterIcon>
+
+        <FooterIcon>
+          <img src={ArrowRightSrc} alt="Arrow next" />
+        </FooterIcon>
+      </FooterIcons>
+    </FooterContainer>
+  );
+}
---
+++ client/src/stories/SplashFooter.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import SplashFooter from "../components/SplashFooter";
+
+export default {
+  title: "SplashFooter",
+  component: SplashFooter,
+};
+
+export const Splash1Footer = () => <SplashFooter />;
